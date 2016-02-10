9a9ddaf204043461d3c0d7719d2142e23dc4847f
luckygun <luckygun01@gmail.com>
2016-02-10 20:46:37 +0100

    Initialisation du plugin

---
+++ src/com/bases/fr/Main.java
@@ -0,0 +1,21 @@
+package com.bases.fr;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin{
+
+	public void onEnable(){
+		System.out.println("Plugin ON");
+	}
+	
+	
+	
+	public void onDisable(){
+		System.out.println("Plugin OFF");
+	}	
+	
+	
+	
+	
+	
+}
