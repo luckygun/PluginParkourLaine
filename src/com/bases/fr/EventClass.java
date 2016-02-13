ef255eebd5cd00f8f7035594285f82a99428e608
luckygun <luckygun01@gmail.com>
2016-02-13 15:01:43 +0100

    Début de détectiion de l'item en main

--- src/com/bases/fr/EventClass.java
+++ src/com/bases/fr/EventClass.java
@@ -2,17 +2,21 @@ package com.bases.fr;
 
 import java.util.Set;
 
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.player.PlayerItemHeldEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class EventClass implements Listener{	//Classe pour gérer les évènements
 
 	private Main plugin;
 	private PlayerLoginEvent log;
-
+	private ItemStack itemInHand;
+	
 	public EventClass(Main main) {
 		this.plugin = main;
 	}
@@ -22,5 +26,20 @@ public class EventClass implements Listener{	//Classe pour gérer les évènements
 	public void onBreakBlock(BlockBreakEvent e){
 		e.setCancelled(true);
 	}
-
+	
+	@EventHandler
+	public void playerItemHold(PlayerItemHeldEvent e){
+		
+		itemInHand = e.getPlayer().getItemInHand();
+		// stone = new ItemStack(Material.STONE);
+		Player p = e.getPlayer();
+		
+		
+		if(itemInHand.getType()==Material.STONE){
+			p.sendMessage("Bonjour");
+			
+		}
+		itemInHand = e.getPlayer().getItemInHand();
+		System.out.println((itemInHand.getType()));
+	}
 }
--- src/com/bases/fr/Main.java
+++ src/com/bases/fr/Main.java
@@ -1,27 +1,33 @@
 package com.bases.fr;
 
+import java.io.File;
 import java.util.Set;
 
 import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.defaults.GameModeCommand;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin{	//classe principale
-
+	
 	private EventClass ec; 				// On crée l'objet EventClass
 	
 	public void onEnable(){				// Fonction au démarrage du plugin
 		
 		ec = new EventClass(this);		// Initialise l'objet EventClass
 		
-		getConfig().options().copyDefaults(false);		// Créé le fichier de config (vide actuellement)
+		File f = new File("plugins\\PluginParkour\\config.yml");
+		
+		getConfig().options().copyDefaults(true);		// Créé le fichier de config 
 		this.saveConfig();
 		
-		PluginManager pm = Bukkit.getServer().getPluginManager();
-		pm.registerEvents(ec, this);
+		
 		System.out.println("Plugin ON");
 	}
 	
@@ -35,7 +41,15 @@ public class Main extends JavaPlugin{	//classe principale
 	public boolean onCommand(CommandSender player, Command cmd, String label, String[] args){
 		Player p = (Player) player;
 		if(cmd.getName().equalsIgnoreCase("startparkour")){
-			p.sendMessage("/startparkour fonctionne !");
+			p.sendMessage("/startparkour fonctionne !");		// Message de test
+			//p.setGameMode(GameMode.ADVENTURE);				// Change le gamemode
+			
+			p.getInventory().setItem(0, new ItemStack(Material.STONE));
+			p.getInventory().setItem(1, new ItemStack(Material.CLAY));
+			p.getInventory().setItem(2, new ItemStack(Material.WOOL));
+			p.updateInventory();								//update l'inventaire
+			PluginManager pm = Bukkit.getServer().getPluginManager();		// "Active" la classe Event
+			pm.registerEvents(ec, this);
 		}
 		
 		
